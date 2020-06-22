999603772ca2cc904fbf55a794be4bf5cf4385f4
lichen <lichenarthurml@gmail.com>
2020-06-22 15:43:14 +0800

    9_map

---
+++ 9_map/2_delete.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+        /* 创建map */
+        countryCapitalMap := map[string]string{"France": "Paris", "Italy": "Rome", "Japan": "Tokyo", "India": "New delhi"}
+
+        fmt.Println("原始地图")
+        printMap(countryCapitalMap)
+
+        /*删除元素*/ 
+        delete(countryCapitalMap, "France")
+        fmt.Println("\n法国条目被删除")
+        printMap(countryCapitalMap)
+}
+
+func printMap(theMap map[string] string) {
+        for key := range theMap {
+                fmt.Println(key, ": ", theMap[key])
+        }
+}
