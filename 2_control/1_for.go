46bd63d5bee6650ff2bd89153d8d2b9c34d0606f
lichen <lichenarthurml@gmail.com>
2020-06-22 14:42:55 +0800

    3_fun

---
+++ 2_control/1_for.go
@@ -0,0 +1,60 @@
+package main
+
+import "fmt"
+
+func main() {
+	// init、position、post 可省略部分
+	sum := 0
+	for i := 0; i <= 10; i++ {
+		sum += i
+	}
+	fmt.Println(sum)
+
+	// 切片
+	strings := []string{"google", "runoob"}
+        for i, s := range strings {
+                fmt.Println(i, s)
+        }
+        numbers := [6]int{1, 2, 3, 5} 
+        for i,x:= range numbers {
+                fmt.Printf("第 %d 位 x 的值 = %d\n", i,x)
+        }
+
+        // label标签，break的地方
+        re:
+        for i := 1; i <= 3; i++ {
+            fmt.Printf("i: %d\n", i)
+            for i2 := 11; i2 <= 13; i2++ {
+                fmt.Printf("i2: %d\n", i2)
+                break re
+            }
+        }
+
+        // continue 也可以用label
+
+	// 测试goto
+	var a int = 10
+	LOOP: for a < 20 {
+		if a == 15 {
+			/* 跳过迭代 */
+			a = a + 1
+			goto LOOP
+		}
+		fmt.Printf("a的值为 : %d\n", a)
+		a++
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # go_sample
 go sample
+
+## 学习地址
+
+https://www.runoob.com/go/go-tutorial.html
